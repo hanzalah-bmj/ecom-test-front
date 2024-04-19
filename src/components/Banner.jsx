8bd4f5e784f41affd2426288e93624876117dd12
hanzalah-bmj <hanzalah.bmj@gmail.com>
2024-04-19 22:35:33 +0500

    changes

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="banner-container">
+      <img
+        src="https://i0.wp.com/clickdukan.com/wp-content/uploads/2023/03/HLBanner.webp?resize=800%2C324&ssl=1" // Replace with your image URL
+        alt="Banner"
+        className="banner-image"
+      />
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import Banner from "./Banner"; // Import the Banner component
+import './style.css';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Banner /> {/* Include the Banner component */}
+      <div className="home-content">
+        {/* Other content of your home page */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
